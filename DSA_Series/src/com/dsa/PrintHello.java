f78bf0f9ef92a4457e4f336174bde479a7f7bdae
alokrai0607 <saelalok001@gmail.com>
2023-12-31 13:02:39 +0530

    Hello

---
+++ DSA_Series/src/com/dsa/PrintHello.java
@@ -0,0 +1,10 @@
+package com.dsa;
+
+public class PrintHello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ DSA_Series/src/com/dsa/SingleLineSingleInteger.java
@@ -0,0 +1,18 @@
+package com.dsa;
+
+import java.util.Scanner;
+
+public class SingleLineSingleInteger {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Pass your input value you will found value+1 in your result");
+		int n = sc.nextInt();
+		System.out.println("Result is : 5");
+		System.out.println(n + 1);
+	}
+
+}
+
+// Input : 4
+// Output : 5
