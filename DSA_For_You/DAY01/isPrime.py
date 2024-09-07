b2536d73ef4e54a53389555c2aa1965700f32984
alokrai0607 <saelalok001@gmail.com>
2024-09-07 12:54:29 +0530

    isPrime

---
+++ DSA_For_You/DAY01/isPrime.py
@@ -0,0 +1,16 @@
+def isPrime(n):
+    if n <= 1:
+        print(n, "Is Not Prime")
+        return
+    
+    count = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            count += 1
+    
+    if count == 2: 
+        print(n, "Is Prime")
+    else:
+        print(n, "Is Not Prime")
+
+isPrime(13)
