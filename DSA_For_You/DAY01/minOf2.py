75e610fe7c1ea2a2a625cdbf30b93f41dd98c936
alokrai0607 <saelalok001@gmail.com>
2024-09-07 12:20:45 +0530

    minOf2

--- DSA_For_You/DAY01/minOf2.py
+++ DSA_For_You/DAY01/minOf2.py
@@ -1,9 +1,19 @@
+# def minOF2(a,b):
+#     if a>b:
+#         print(a,"Is Grater than",b)
+#     elif b>a:
+#         print(b,"Is Grater than",a)
+#     else:
+#         print("I Hope Both are true")
+
+# minOF2(25,28)
+
 def minOF2(a,b):
     if a>b:
-        print(a,"Is Grater than",b)
+        print(b,"Is Minimum")
     elif b>a:
-        print(b,"Is Grater than",a)
+        print(a,"Is Minimum")
     else:
         print("I Hope Both are true")
 
-minOF2(25,28)
+print(minOF2(25,28))
