080b4d2828dc03f2308f34948e43e6c8bef5fb62
alokrai0607 <saelalok001@gmail.com>
2024-09-07 15:38:39 +0530

    isLowerOrUpper

--- DSA_For_You/DAY03/ConditionalSt_Loops/CharisLowerOrUpper.py
+++ DSA_For_You/DAY03/ConditionalSt_Loops/CharisLowerOrUpper.py
@@ -6,3 +6,13 @@ def isLowerOrUpper(ch:str):
     
 print(isLowerOrUpper("a"))
 print(isLowerOrUpper("A"))
+
+#2 nd Way
+def isLowerOrUpper(ch:str):
+    if(ch>="a" and ch<="z"):
+        return (ch,"Is in Lower Case")
+    elif(ch>="A" and ch<="Z"):
+        return (ch,"Is in Upper Case")
+    
+print(isLowerOrUpper("a"))
+print(isLowerOrUpper("A"))
