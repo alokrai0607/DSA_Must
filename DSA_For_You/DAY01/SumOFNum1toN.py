b6c9a3f237ea65012d4b26230dedcd30bbed3296
alokrai0607 <saelalok001@gmail.com>
2024-09-07 12:41:53 +0530

    SumOfN

--- DSA_For_You/DAY01/SumOFNum1toN.py
+++ DSA_For_You/DAY01/SumOFNum1toN.py
@@ -1,7 +1,8 @@
 def sumofNnumbers(n):
     sum = 0 
-    for i in range(1,n):
+    for i in range(1,n+1):
         sum += i
-    return sum
+    return(sum)
 
 print(sumofNnumbers(25))
+print(sumofNnumbers(5))
