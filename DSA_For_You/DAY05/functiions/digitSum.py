87f7906951e0dbcec5e8c393f28f7354779250e5
alokrai0607 <saelalok001@gmail.com>
2024-09-10 12:26:35 +0530

    difSum

---
+++ DSA_For_You/DAY05/functiions/digitSum.py
@@ -0,0 +1,11 @@
+def digitSum(num):
+    digSum=0
+    while (num>0):
+        last_digit = num % 10
+        num //= 10
+        digSum += last_digit
+    return digSum
+
+print(round(digitSum(12345)))
+
+
